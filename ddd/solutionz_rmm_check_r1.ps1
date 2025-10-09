a1fcdbbe7cc230ce2f4c2cbc8ba1869fa952422a
dddsolutionz <dddsolutionz@gmail.com>
2025-10-09 01:05:16 -0400

    Update solutionz_rmm_check_r1.ps1

--- ddd/solutionz_rmm_check_r1.ps1
+++ ddd/solutionz_rmm_check_r1.ps1
@@ -194,6 +194,9 @@ if ($success) {
     Write-Host ""
     Write-Host "Please email this file to: rmmadmins@solutionzinc.com"
     Write-Host "Subject: RMM Connectivity Report from $(hostname)"
+    Write-Host ""
+    Write-Host "Thank you again for your time and support in verifying your connection."
+    Write-Host "Have a wonderful day! ツ"
 } else {
     Write-Host "ZIP file creation failed."
     Write-Host "Please manually attach the log file located at:"
